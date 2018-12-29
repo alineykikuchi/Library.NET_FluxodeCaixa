7a0505d4e230506bc40c083349e9dab9cb70db71
Aline Yumi Kikuchi <aline.kikuchi@benner.com.br>
2018-12-29 20:49:11 -0200

    testando comandos

--- Library_FluxoCaixa/EnumExtension.cs
+++ Library_FluxoCaixa/EnumExtension.cs
@@ -10,19 +10,19 @@ namespace Library_FluxoCaixa
 {
     public class EnumExtension
     {
-        public static string GetDescription(System.Enum value)
-        {
-            FieldInfo fi = value.GetType().GetField(value.ToString());
-            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
-            if (attributes.Length > 0)
-            {
-                return attributes[0].Description;
-            }
-            else
-            {
-                return value.ToString();
-            }
-        }
+        //public static string GetDescription(System.Enum value)
+        //{
+        //    FieldInfo fi = value.GetType().GetField(value.ToString());
+        //    DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+        //    if (attributes.Length > 0)
+        //    {
+        //        return attributes[0].Description;
+        //    }
+        //    else
+        //    {
+        //        return value.ToString();
+        //    }
+        //}
         public static List<string> GetDatavalues(System.Enum theEnum)
         {
             List<string> lstRetorno = null;
